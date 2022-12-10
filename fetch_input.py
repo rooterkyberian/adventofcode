e37c6bbda1d5d43bcb2840e2f0df82626d4b9b2e
Maciej Urbanski <rooter@kyberian.net>
2022-12-10 22:05:29 +0100

    input fetcher

---
+++ fetch_input.py
@@ -0,0 +1,35 @@
+import json
+from datetime import date
+
+import requests
+
+from pathlib import Path
+
+
+def fetch_input(year, day, session):
+    input_path = Path(f"{year}/{day:02d}/input.txt")
+    if not input_path.exists():
+        input_path.parent.mkdir(parents=True, exist_ok=True)
+        url = f"https://adventofcode.com/{year}/day/{day}/input"
+        with input_path.open("w") as f:
+            resp = requests.get(url, cookies={"session": session})
+            resp.raise_for_status()
+            f.write(resp.text)
+    return input_path
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("year", type=int, default=date.today().year, nargs="?")
+    args = parser.parse_args()
+    with open(".adventofcode.json") as f:
+        session = json.load(f)["session"]
+
+    for day in range(1, 26):
+        fetch_input(args.year, day, session).read_text()
+
+
+if __name__ == "__main__":
+    main()
